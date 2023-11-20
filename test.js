213e968ac0f199346950bf818593a09475aa2bfb
lenin254 <leninxarebalo18@hotmail.com>
2023-11-20 12:44:09 -0500

    acabo la clase

---
+++ test.js
@@ -0,0 +1,14 @@
+if(undefined == null) {
+    alert("undefined == null -> true");
+}
+else {
+    alert("undefined == null -> false");
+}
+
+if(NaN == NaN) {
+    alert("NaN == NaN -> true");
+}
+else {
+    alert("NaN == NaN -> false");
+}
+
